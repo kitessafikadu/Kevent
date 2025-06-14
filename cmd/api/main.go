79dca4e7b6453859ed31f0c896a63b0064408877
Kitessa Fikadu <kitessafikadu@gmail.com>
2025-06-14 21:38:26 +0300

    Setup Gin server

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,9 +1,20 @@
 package main
 
-import(
+import (
 	"database/sql"
 	"log"
+
+	"github.com/kitessafikadu/kevent/internal/database"
+	"github.com/kitessafikadu/kevent/internal/env"
+	_ "github.com/joho/godotenv/autoload"
+	_ "github.com/mattn/go-sqlite3" // Import SQLite driver
 )
+
+type application struct {
+	port     int
+	jwtSecret string
+	models database.Models
+}
 
 func main(){
 	db,err := sql.Open("sqlite3", "./data.db")
@@ -12,4 +23,15 @@ func main(){
 	}
 
 	defer db.Close()
+
+	models := database.NewModels(db)
+	app := &application{
+		port: env.GetEnvInt("PORT", 8080),
+		jwtSecret: env.GetEnvString("JWT_SECRET", "some-secret-123456"),
+		models: models,
+	}
+
+	if err := app.serve(); err != nil {
+		log.Fatal(err)
+	}
 }
