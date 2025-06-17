a984d5fccc9ea8120ad53087c0bbdc1b2f169ee6
Kitessa Fikadu <kitessafikadu@gmail.com>
2025-06-17 12:00:15 +0300

    Enhance event handling: add error responses for create, get, and update events

--- cmd/api/events.go
+++ cmd/api/events.go
@@ -1,1 +1,74 @@
 package main
+
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+	"github.com/kitessafikadu/kevent/internal/database"
+)
+
+func (app *application) createEvent (c *gin.Context){
+	var event database.Event
+
+	if err := c.ShouldBindJSON(&event); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	err:= app.models.Events.insert(&event)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to create event"})
+		return
+	}
+	c.JSON(http.StatusCreated, event)
+}
+
+func (app *application) getEvent(c *gin.Context){
+	id, err:=strconv.Atoi(c.Param("id"))
+
+	if err != nil{
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid event ID"})
+	}
+	event,err:= app.models.Events.get(id)
+	if err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "Event not found"})
+	}
+
+	if err != nil{
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to retrieve event"})
+	}
+	c.JSON(http.StatusOK, event)
+
+	func (app *application) updateEvent(c *gin.Context) {
+	id, err := strconv.Atoi(c.Param("id"))
+
+	if err != nil{
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid event ID"})
+		return
+	}
+
+	existingEvent, err := app.models.Events.Get(id)
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to retrieve event"})
+		return
+	}
+
+	if existingEvent == nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "Event not found"})
+		return
+	}
+
+	updatedEvent = &database.Event{}
+	if err := c.ShouldBindJSON(updatedEvent); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	updatedEvent.Id = id
+	if err := app.models.Events.Update(updatedEvent); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to update event"})
+		return
+	}
+	c.JSON(http.StatusOK, updatedEvent)
+}
