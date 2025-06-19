f3a881e274aaad02065c5d4180ad2bbd3debc18a
Kitessa Fikadu <kitessafikadu@gmail.com>
2025-06-19 12:48:24 +0300

    Implement user registration handler with password hashing and database insertion

--- cmd/api/auth.go
+++ cmd/api/auth.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"net/http"
+	"github.com/kitessafikadu/kevent/internal/database"
+
 	"github.com/gin-gonic/gin"
+	"golang.org/x/crypto/bcrypt"	
 )
 
 type registerRequest struct {
@@ -11,4 +14,30 @@ type registerRequest struct {
 	Name		string `json:"name" binding:"required,min=3"`
 }
 
+func (app *application) registerUser(c *gin.Context){
+	var register registerRequest
+	if err := c.ShouldBindJSON(&register); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
 
+	hashedPassword,err:=bcrypt.GenerateFromPassword([]byte(register.Password), bcrypt.DefaultCost)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to hash password"})
+		return
+	}
+
+	// Create the user in the database
+	register.Password = string(hashedPassword)
+	user := database.User{
+		Email:    register.Email,
+		Password: register.Password,
+		Name:     register.Name,
+	}
+	if err := app.models.Users.Insert(&user); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to create user"})
+		return
+	}
+
+	c.JSON(http.StatusCreated, gin.H{"message": "User registered successfully"})
+}
